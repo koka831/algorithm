7fa80b5117b1c71b8a26d16481a469c79f97a611
koka <koka.code@gmail.com>
2023-12-04 22:49:00 +0900

    aoc2023 day4

---
+++ advent-of-code-2023/src/bin/4_1.rs
@@ -0,0 +1,46 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+/// https://adventofcode.com/2023/day/4
+/// Day 4: Scratchcards
+fn solve(path: &str) -> io::Result<usize> {
+    let file = File::open(path)?;
+    let lines = BufReader::new(&file).lines();
+
+    let mut count = 0;
+
+    for line in lines {
+        let line = line?;
+        let (winner, mine) = line[line.find(':').unwrap() + 1..]
+            .split_once('|')
+            .map(|(winner, mine)| (parse(winner), parse(mine)))
+            .unwrap();
+
+        let point = mine.iter().filter(|m| winner.contains(m)).count();
+        count += 2usize.pow(point as u32) / 2;
+    }
+
+    Ok(count)
+}
+
+fn parse(s: &str) -> Vec<usize> {
+    s.split_whitespace()
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect::<Vec<_>>()
+}
+
+fn main() {
+    println!("{}", solve("./input/day4_1.txt").unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(solve("./input/day4_1.sample.txt").unwrap(), 13);
+    }
+}
---
+++ advent-of-code-2023/src/bin/4_2.rs
@@ -0,0 +1,47 @@
+use std::{fs::File, io};
+
+/// https://adventofcode.com/2023/day/4
+/// Day 4: Scratchcards
+fn solve(path: &str) -> io::Result<usize> {
+    let file = File::open(path)?;
+    let input = std::io::read_to_string(file)?;
+    let lines = input.trim().split('\n').collect::<Vec<_>>();
+
+    let mut cards = vec![1; lines.len()];
+
+    for (i, line) in lines.iter().enumerate() {
+        let (winner, mine) = line[line.find(':').unwrap() + 1..]
+            .split_once('|')
+            .map(|(winner, mine)| (parse(winner), parse(mine)))
+            .unwrap();
+
+        let point = mine.iter().filter(|m| winner.contains(m)).count();
+        for j in 0..point {
+            cards[i + j + 1] += cards[i];
+        }
+    }
+
+    let total = cards.iter().sum::<usize>();
+
+    Ok(total)
+}
+
+fn parse(s: &str) -> Vec<usize> {
+    s.split_whitespace()
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect::<Vec<_>>()
+}
+
+fn main() {
+    println!("{}", solve("./input/day4_1.txt").unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(solve("./input/day4_1.sample.txt").unwrap(), 30);
+    }
+}
